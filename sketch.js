f84bb3b6a7ba8339dccb3685520c08a98e86eb73
unknown <ben.norman34@gmail.com>
2021-03-15 20:34:50 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,16 +28,16 @@ function setup() {
   createCanvas(600, 600);
   background(200);
 
-randomIndex = int(random(gods.length));
-
-console.log(gods[randomIndex].name);
-text(gods[randomIndex].name, 50, 50);
-
-gods.splice(randomIndex, 1);
-console.log(gods);
 
 }
 
 function draw() {
 
+}
+
+function mousePressed() {
+  background(random(255));
+  randomIndex = int(random(gods.length));
+  text(gods[randomIndex].name, 50, 50);
+  gods.splice(randomIndex, 1);
 }
