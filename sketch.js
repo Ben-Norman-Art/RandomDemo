e5a102da5292c3f1d09257643204ea029a5b594a
unknown <ben.norman34@gmail.com>
2021-03-15 20:52:19 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ let randomIndex;
 function setup() {
   createCanvas(600, 600);
   background(200);
-
+  textSize(32);
 
 }
 
@@ -36,8 +36,13 @@ function draw() {
 }
 
 function mousePressed() {
+
+if (dogs[0]){
   background(random(200, 255));
   randomIndex = int(random(gods.length));
   text(gods[randomIndex].name, 50, 50);
   gods.splice(randomIndex, 1);
+} else {
+  text("Nothing left!", 50, 50);
+}
 }
