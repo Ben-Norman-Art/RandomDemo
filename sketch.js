46d2b5c9b8b2e2c15c47f985affc6fae3f7f3bf1
unknown <ben.norman34@gmail.com>
2021-03-21 22:53:46 -0700

    Finalized Touches

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 let gods = [{
   name: "Anubis",
   weapon: "Flail"
@@ -46,58 +45,75 @@ let gods = [{
 let randomIndex;
 let animating = false;
 let egyptian = [];
+let imageCounter = 0;
 let button;
 
-function preload(){
+function preload() {
 
-  for (let i = 0; i <= 13; i++)
-  egyptian[1] = loadImage(`Assets/Egyptian_${i}.JPG`)
-
+  for (let i = 0; i <= 9; i++) {
+    egyptian[i] = loadImage(`Assets/Egyptian_${i}.PNG`)
+  }
 }
 
 function setup() {
-  var canvas = createCanvas(550, 550);
+  var canvas = createCanvas(600, 600);
   canvas.parent('sketch-holder');
   button = createButton('Press Me!!!');
   button.parent('button-holder');
   button.mousePressed(buttonPressed);
-  background(200);
+  button.class("randomizerButton");
+  background(247, 210, 158);
   textSize(32);
+  imageMode(CENTER);
+  frameRate(8);
 
+  textFont("Optima");
   text("Learn About the Egyptian Gods", 50, 50);
-  console.log(egyptian);
+
 
 }
 
 function draw() {
 
 
-  if(animating == true){
+
+  if (animating == true) {
+    background(255, 230, 194);
+
     let r = random(0, 255);
-    let r2 = random(0);
+    let r2 = random(0, 255);
     let r3 = random(0, 255);
     let c = color(r, 0, r3);
     fill(c);
     rect(random(width), random(height), random(50, 200));
 
-
+    image(egyptian[imageCounter], width / 2, height / 2);
+    if (imageCounter < egyptian.length - 1) {
+      imageCounter++;
+    } else {
+      imageCounter = 0;
+    }
   }
 }
 
-function randomizer(){
+function randomizer() {
   animating = false;
 
-  if (gods[0]){
-    background(random(200, 255));
+
+  if (gods[0]) {
+    background(255, 230, 194);
     randomIndex = int(random(gods.length));
+    textFont("Optima");
     text(`${gods[randomIndex].name}'s God Weapon is the
-      ${gods[randomIndex].weapon}`, 50, 50);
+      ${gods[randomIndex].weapon}`, width/2 - 150, height/2 + 150);
+      image(random(egyptian), width/2, height / 2);
     // text(gods[randomIndex].name + "'s God Weapon is the " + gods[randomIndex].weapon, 50, 50);
     gods.splice(randomIndex, 1);
   } else {
     background(random(200, 255));
     text("Nothing left!", 50, 50);
   }
+
 }
 
 function buttonPressed() {
